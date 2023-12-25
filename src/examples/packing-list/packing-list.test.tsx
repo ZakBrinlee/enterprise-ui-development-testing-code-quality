4a06b371c8f2b5e56f75bd4d6759a1ce13228e64
Zak Brinlee <zbrinlee@hotmail.com>
2023-12-24 21:54:22 -0800

    component testing completed

--- src/examples/packing-list/packing-list.test.tsx
+++ src/examples/packing-list/packing-list.test.tsx
@@ -1,28 +1,66 @@
-import { render, screen } from 'test/utilities';
-import PackingList from '.';
+import { render as _render, screen } from 'test/utilities';
+import {PackingList} from '.';
+import { createStore } from './store';
+import { Provider } from 'react-redux';
+
+const renderWithProvider = (ui: React.ReactElement) => {
+  return _render(ui, { wrapper: ({ children }) => <Provider store={createStore()}>{children}</Provider> });
+}
 
 it('renders the Packing List application', () => {
-  render(<PackingList />);
+  renderWithProvider(<PackingList />);
 });
 
 it('has the correct title', async () => {
-  render(<PackingList />);
+  renderWithProvider(<PackingList />);
   screen.getByText('Packing List');
 });
 
-it.todo('has an input field for a new item', () => {});
+it('has an input field for a new item', () => {
+  renderWithProvider(<PackingList />);
+  screen.getByLabelText('New Item Name');
+});
 
-it.todo(
+it(
   'has a "Add New Item" button that is disabled when the input is empty',
-  () => {},
+  () => {
+    renderWithProvider(<PackingList />);
+    const input = screen.getByLabelText('New Item Name');
+    const button = screen.getByRole('button', { name: /add new item/i });
+    
+    expect(input).toHaveValue('');
+    expect(button).toBeDisabled();
+  },
 );
 
-it.todo(
+it(
   'enables the "Add New Item" button when there is text in the input field',
-  async () => {},
+  async () => {
+    const {user} = renderWithProvider(<PackingList />);
+    const input = screen.getByLabelText('New Item Name');
+    const button = screen.getByRole('button', { name: /add new item/i });
+    
+    expect(input).toHaveValue('');
+
+    await user.type(input, 'Socks');
+
+    expect(button).not.toBeDisabled();
+  },
 );
 
-it.todo(
+it(
   'adds a new item to the unpacked item list when the clicking "Add New Item"',
-  async () => {},
+  async () => {
+    const {user} = renderWithProvider(<PackingList />);
+    const input = screen.getByLabelText('New Item Name');
+    const button = screen.getByRole('button', { name: /add new item/i });
+    
+    expect(input).toHaveValue('');
+
+    await user.type(input, 'socks');    
+    await user.click(button);
+
+    expect(screen.getByLabelText('socks')).not.toBeChecked();
+    expect(input).toHaveValue(''); 
+  },
 );
